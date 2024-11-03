111ccccbaae34318a81bda518a0a1dc329961720
@Nicole <kagurunicky@gmail.com>
2024-11-03 16:12:42 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+intro = "USER DETAILS PROGRAM "
+name = input("Please enter your Name: ")
+age = input("Please enter your Age: ")
+location = input("Please enter your Current Address: ")
+print(intro)
+print(f"Hello,{name} Welcome to our System you are {age} years Old and your address is {location}")
